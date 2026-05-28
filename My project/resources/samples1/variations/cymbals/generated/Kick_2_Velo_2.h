c001dbf468f53604b78953dba609c50d6463e795
Saske2201 <saske2201@gmail.com>
2026-05-28 17:51:01 +0300

    cpp h upd

---
+++ My project/resources/samples1/variations/cymbals/generated/Kick_2_Velo_2.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdint.h>
+
+#ifndef RESOURCE_T_DEFINED
+#define RESOURCE_T_DEFINED
+struct resource_t {
+  resource_t(const char* name, const uint8_t* data, uint32_t size) : name(name), data(data), size(size) {}
+  const char* name; const uint8_t* data; const uint32_t size;
+};
+#endif
+
+extern const uint8_t Kick_2_Velo_2_data[397590];
+extern const int Kick_2_Velo_2_data_length;
+
